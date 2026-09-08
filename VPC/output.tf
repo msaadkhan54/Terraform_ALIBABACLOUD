9f75313a7445b96d8b3f60a07be0ae48006978fc
Muhammad Saad Khan <apexsaad54@gmail.com>
2026-09-08 22:55:26 +0500

    Add ACK and VPC infrastructure modules

--- VPC/output.tf
+++ VPC/output.tf
@@ -1,3 +1,7 @@
 output "vpc_id" {
   value = alicloud_vpc.main_vpc.id
 }
+
+output "vpc" {
+  value= alicloud_vpc.main_vpc.name
+}
--- output.tf
+++ output.tf
@@ -1,3 +1,6 @@
 output "ack_cluster_id" {
   value = module.ack.cluster_id
 }
+output "vpc" {
+  value= alicloud_vpc.main_vpc.name
+}
